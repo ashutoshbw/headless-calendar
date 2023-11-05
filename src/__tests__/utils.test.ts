f4f4193d0f243b514524e7dad450b47d4eb02f76
Ashutosh Biswas <ashutoshbw314@gmail.com>
2023-11-06 01:50:29 +0600

    test: covered getLocaleDateArray

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,12 +1,20 @@
 import { it, expect, describe } from 'vitest';
 
-import { FullDate, convertToJSDate } from '../utils';
+import { FullDate, convertToJSDate, getLocaleDateArray } from '../utils';
 
 describe('convertToJSDate()', () => {
   it('month number should start with 1', () => {
     const monthNumber = 1;
     const fullDate: FullDate = [2023, monthNumber, 1];
-    const resultDate = convertToJSDate(fullDate);
-    expect(resultDate.getUTCMonth()).toBe(monthNumber - 1); // in JS Date, month starts with 0
+    const result = convertToJSDate(fullDate);
+    expect(result.getUTCMonth()).toBe(monthNumber - 1); // in JS Date, month starts with 0
+  });
+});
+
+describe('getLocaleDateArray()', () => {
+  it('month number should start with 1', () => {
+    const now = new Date();
+    const result = getLocaleDateArray(now);
+    expect(result[1]).toBe(now.getMonth() + 1);
   });
 });
